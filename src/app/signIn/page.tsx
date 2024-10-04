ba675b7c3467fe97e55ea5fa661133e136ca4694
Leonardo Mazolini Fernandes <77203283+LeoMFern@users.noreply.github.com>
2024-10-03 21:53:12 -0300

    Login form

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 
 import { LoginWithGoogleButton } from "./_components/login-with-google-button";
+import Image from "next/image";
+import CardWine from "~/components/card-wine";
+import Stats from "~/components/stats";
+import { Button } from "~/components/ui/button";
+import { type getWines } from "~/server/db/select";
+import Link from "next/link";
+import { serialize } from "~/app/products/_components/nuqs-parsers";
 
 export default function SignInPage() {
   return (
@@ -15,6 +22,38 @@ export default function SignInPage() {
               Agnello
             </h1>
           </div>
+          <form className=" pt-4">
+            <div className="mb-4">
+                <label className="block text-[#6d071a] text-sm font-bold mb-2" htmlFor="email">
+                    Email
+                </label>
+                <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-[#6d071a] leading-tight focus:outline-none focus:shadow-outline"
+                    id="email"
+                    type="email"
+                    placeholder="Digite seu email"
+                /> 
+            </div>
+            <div className="mb-6">
+                <label className="block text-[#6d071a] text-sm font-bold mb-2" htmlFor="password">
+                    Senha
+                </label>
+                <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    id="password"
+                    type="password"
+                    placeholder="Digite sua senha"
+                />
+                
+            </div>
+            <div className="flex flex-col items-center">
+              <Link href="/products">
+                <Button variant={"default"} size={"lg"}>
+                  Entrar
+                </Button>
+              </Link>
+            </div>
+        </form>
           <div className="space-y-4">
             <LoginWithGoogleButton />
           </div>
